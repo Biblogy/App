305482fd415706cff3c8fc079788ecc1aba48f41
Veit Progl <->
2020-11-22 20:26:13 +0100

    first commit

---
+++ Shared/Shema/ResponseType.swift
@@ -0,0 +1,8 @@
+//
+//  ResponseType.swift
+//  EBookTracking
+//
+//  Created by Veit Progl on 21.11.20.
+//
+
+import Foundation
---
+++ Shared/Sidebar.swift
@@ -0,0 +1,20 @@
+//
+//  Sidebar.swift
+//  EBookTracking
+//
+//  Created by Veit Progl on 17.11.20.
+//
+
+import SwiftUI
+
+struct Sidebar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Sidebar_Previews: PreviewProvider {
+    static var previews: some View {
+        Sidebar()
+    }
+}
