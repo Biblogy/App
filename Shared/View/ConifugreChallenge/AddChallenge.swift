a22692c9b91f1a0acb1e173d2199096cb36b7c25
Veit Progl <->
2020-12-10 20:58:09 +0100

    Add Challenge

--- Shared/View/ConifugreChallenge/AddChallenge.swift
+++ Shared/View/ConifugreChallenge/AddChallenge.swift
@@ -7,18 +7,28 @@
 
 import SwiftUI
 
+class challengeData: ObservableObject {
+    @Published var bookID: String = "-404"
+    @Published var bookTitle: String
+    @Published var book: Book?
+    
+    init(bookTitle: String) {
+        self.bookTitle = bookTitle
+    }
+}
+
 struct AddChallenge: View {
-    @State private var doesStuff = ""
+    @State private var time = ""
     @State private var selected = 1
     @State private var menuTime: time = .days
     @Binding var isOpen: Bool
 
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Book.title, ascending: true)], animation: .default)
+        sortDescriptors: [NSSortDescriptor(keyPath: \Book.title, ascending: true)], predicate: NSPredicate(format: "done != true"), animation: .default)
     private var items: FetchedResults<Book>
     
-//    @State private var selectedBook = 
+    @ObservedObject var data = challengeData(bookTitle: "select a book")
     
     var body: some View {
         NavigationView() {
@@ -30,11 +40,11 @@ struct AddChallenge: View {
                 .pickerStyle(SegmentedPickerStyle())
                 .frame(width: 200)
                 
-                Text("I will read").font(.title).bold()
+                Text("I will read").font(.title).bold().frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                 HStack() {
                     Text("for").font(.title).bold()
                     Spacer()
-                    TextField("10", text: $doesStuff)
+                    TextField("10", text: $time)
                     Menu(self.menuTime.rawValue) {
                         Button("days") { self.menuTime = .days }
                         Button("weeks") { self.menuTime = .weeks }
@@ -45,17 +55,26 @@ struct AddChallenge: View {
                 HStack() {
                     Text("the book:").font(.title).bold()
                     Spacer()
-                    Menu(self.menuTime.rawValue) {
-                        Button("days") { self.menuTime = .days }
-                        Button("weeks") { self.menuTime = .weeks }
-                        Button("month") { self.menuTime = .month }
-                        Button("years") { self.menuTime = .years }
+                    Menu(self.data.bookTitle) {
+                        Button(action: {
+                            self.data.bookTitle = "select a book"
+                            self.data.bookID = "-404"
+                            self.data.book = nil
+                            print(self.data)
+                        }, label: {
+                            Text("select a book")
+                        })
+                        ForEach(items) { book in
+                            Button(action: {
+                                self.data.bookTitle = book.title ?? "error"
+                                self.data.bookID = book.id ?? "-404"
+                                self.data.book = book
+                                print(self.data)
+                            }, label: {
+                                Text("\(book.title ?? "error")")
+                            })
+                        }
                     }
-                    Button(action: {
-                        
-                    }, label: {
-                        Image(systemName: "xmark")
-                    })
                 }
                 Spacer()
             }
@@ -71,7 +90,9 @@ struct AddChallenge: View {
                 
                 ToolbarItem(placement: ToolbarItemPlacement.confirmationAction) {
                     Button(action: {
-                        
+                        if self.data.book != nil {
+                            addChallenge(bookId: data.bookID, time: Int16(time) ?? -404, timeType: menuTime.rawValue, book: self.data.book!)
+                        }
                     }, label: {
                         Text("Add")
                     })
@@ -80,6 +101,25 @@ struct AddChallenge: View {
             .padding([.top, .leading, .trailing], 20)
         }
         .frame(minWidth: 0, idealWidth: 500, maxWidth: .infinity, minHeight: 300, maxHeight: .infinity)
+    }
+
+    func addChallenge(bookId: String, time: Int16, timeType: String, book: Book) {
+        let newItem = Challenges(context: viewContext)
+        newItem.id = UUID().uuidString
+        newItem.time = time
+        newItem.timeType = timeType
+        newItem.start = Date()
+        newItem.book = bookId
+        newItem.challengeBook = book
+
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
     }
 }
 //struct AddChallenge_Previews: PreviewProvider {
--- Shared/View/MainViews/ChallengeView.swift
+++ Shared/View/MainViews/ChallengeView.swift
@@ -13,31 +13,44 @@ struct ChallengeView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Book.progress, ascending: true)],
-        predicate: NSPredicate(format: "done == false"), animation: .default)
+        sortDescriptors: [NSSortDescriptor(keyPath: \Challenges.start, ascending: false)], animation: .default)
         
-    private var items: FetchedResults<Book>
+    private var items: FetchedResults<Challenges>
     @State private var openAdd = false
     
     var body: some View {
         VStack() {
-            Picker("", selection: $selected) {
-                        Text("Auto")
-                        Text("Light")
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .frame(width: 200)
-            Text("Hello, World!")
-            
             Button(action: {
                 openAdd.toggle()
             }, label:{
                 Text("Add Chllenge")
             })
             Spacer()
+            
+            List() {
+                ForEach(items) { item in
+                    Text(String(item.challengeBook?.title ?? "error"))
+                }.onDelete(perform: deleteItems)
+            }
         }.sheet(isPresented: self.$openAdd, content: {
             AddChallenge(isOpen: self.$openAdd)
         })
+        .padding([.top], 10)
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
     }
 }
 
