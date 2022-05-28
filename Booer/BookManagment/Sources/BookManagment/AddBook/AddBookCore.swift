147018ad866d1a4b6e2b30573136d8a0d3ccfa58
Veit Progl <veit-progl@t-online.de>
2022-05-28 14:41:12 +0200

    call different effect after books loaded

--- Booer/BookManagment/Sources/BookManagment/AddBook/AddBookCore.swift
+++ Booer/BookManagment/Sources/BookManagment/AddBook/AddBookCore.swift
@@ -14,6 +14,7 @@ public extension AddBookCore {
     struct State: Equatable {
         public init() {}
         var bookDetail = BookDetailCore.State()
+        var books: [Book] = []
         
     }
     
@@ -21,6 +22,7 @@ public extension AddBookCore {
         case onAppear
         case bookDetail(BookDetailCore.Action)
         case requestBook(String)
+        case loadedBooks([Book])
     }
     
     struct Environment {
@@ -33,59 +35,56 @@ public extension AddBookCore {
             case .onAppear:
                 return .none
             case .requestBook(let title):
-                return .result {
-                    let urlString = title.replacingOccurrences(of: " ", with: "%20")
-                    guard let url = URL(string: "http://49.12.191.116/book?title=\(urlString)") else {
-                        return .success(.onAppear)
-                    }
-                    
-                    print(url)
-                    var messageG: [Book]!
-                    let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                        if let error = error {
-                            print(error)
-                            return
-                        }
-                        if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
-                            print(response)
-                            return
-                        }
-                        
-                        guard let data = data else {
-                            print(data)
-                            return
-                        }
-                        
-                        do {
-                            let decoder = JSONDecoder()
-                            var message = try decoder.decode([Book].self, from: data)
-                            messageG = message
-                            print(message)
-                        } catch let err {
-                            print(err)
-                        }
-                    }
+                let urlString = title.replacingOccurrences(of: " ", with: "%20")
+                guard let url = URL(string: "http://49.12.191.116/book?title=\(urlString)") else {
+                    return .none
+                }
+                return Effect.future { promise in
+                  let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+                      guard let data = data else {
+                          promise(.success(.onAppear))
+                          return
+                      }
+                      do {
+                          let books = try JSONDecoder().decode([Book].self, from: data)
+                          promise(.success(.loadedBooks(books)))
+                      } catch {
+                          promise(.success(.onAppear))
+                      }
+                      
+                      promise(.success(.onAppear))
+                  }
                     task.resume()
-                    
-                    return .success(.onAppear)
                 }
             case .bookDetail:
+                return .none
+            case .loadedBooks(let books):
+                state.books = books
                 return .none
             }
         }
     )
 }
 
-struct Book: Decodable {
+public struct Book: Decodable, Equatable {
     var title: String
     var pageCount: Int?
     var publisher: String?
     var author: String?
     var subtitle: String?
+    var id = UUID().uuidString
     
     enum CodingKeys: String, CodingKey {
         case pageCount = "pages"
         case title = "title"
         case subtitle = "subtitle"
-   }
+    }
+    
+    public init(title: String, pageCount: Int?, publisher: String?, author: String?, subtitle: String?) {
+        self.title = title
+        self.pageCount = pageCount
+        self.publisher = publisher
+        self.author = author
+        self.subtitle = subtitle
+    }
 }
